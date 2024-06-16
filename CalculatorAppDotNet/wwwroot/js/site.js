e6eaf0a2a1a98f69eb3d23168c2853d69db8f62a
LIU Heyang <lhypds@gmail.com>
2024-06-16 14:43:34 +0900

    Fix indent

--- CalculatorAppDotNet/wwwroot/js/site.js
+++ CalculatorAppDotNet/wwwroot/js/site.js
@@ -4,69 +4,69 @@ let displayValue = '';
 
 
 function initCalculator() {
-    displayValue = '0';
-    updateDisplay();
-    memory = 0;
-    currentOperation = '';
+  displayValue = '0';
+  updateDisplay();
+  memory = 0;
+  currentOperation = '';
 }
 
 function clearDisplay() {
-    displayValue = '0';
-    updateDisplay();
+  displayValue = '0';
+  updateDisplay();
 }
 
 function updateDisplay() {
-    document.getElementById('display').value = displayValue;
+  document.getElementById('display').value = displayValue;
 }
 
 function appendNumber(number) {
-    // Limit the number of characters in the display to 20
-    if (displayValue.length < 20) {
-        if (displayValue === '0') {
-            if (number === '00') {
-                number = '0';
-            }
-            displayValue = number;
-        } else {
-            displayValue += number;
-        }
-        updateDisplay();
+  // Limit the number of characters in the display to 20
+  if (displayValue.length < 20) {
+    if (displayValue === '0') {
+      if (number === '00') {
+        number = '0';
+      }
+      displayValue = number;
+    } else {
+      displayValue += number;
     }
+    updateDisplay();
+  }
 }
 
 function appendOperation(operation) {
-    if (displayValue !== '') {
-        displayValue += operation;
-        updateDisplay();
-    }
+  if (displayValue !== '') {
+    displayValue += operation;
+    updateDisplay();
+  }
 }
 
 function calculateResult() {
-    try {
-        displayValue = eval(displayValue).toString();
-        updateDisplay();
-    } catch (e) {
-        alert('Invalid expression');
-    }
+  try {
+    displayValue = eval(displayValue).toString();
+    updateDisplay();
+  } catch (e) {
+    alert('Invalid expression');
+  }
 }
 
 function memoryAdd() {
-    memory += parseFloat(displayValue);
-    clearDisplay();
+  memory += parseFloat(displayValue);
+  clearDisplay();
 }
 
 function memorySubtract() {
-    memory -= parseFloat(displayValue);
-    clearDisplay();
+  memory -= parseFloat(displayValue);
+  clearDisplay();
 }
 
 function memoryClear() {
-    displayValue = memory.toString();
-    updateDisplay();
+  displayValue = memory.toString();
+  updateDisplay();
 }
 
 function clearAll() {
-    memory = 0;
-    currentOperation = '';
-    clearDisplay();
+  memory = 0;
+  currentOperation = '';
+  clearDisplay();
 }
