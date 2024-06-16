1ca335526915ff4693dc0920fd4d40e85f78464d
LIU Heyang <lhypds@gmail.com>
2024-06-16 19:48:09 +0900

    Add icons

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function createWindow() {
             contextIsolation: false
         },
         frame: false,  // No frame to make the whole window draggable
+        icon: path.join(__dirname, 'build/icons/icon.png')
     });
 
     // Load the URL served by the .NET backend
