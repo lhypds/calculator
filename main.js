4a252befc6c4c87d670c48fed0900f2d6183496d
LIU Heyang <lhypds@gmail.com>
2024-06-17 07:09:59 +0900

    Small fix for macOS

--- main.js
+++ main.js
@@ -41,16 +41,19 @@ app.whenReady().then(() => {
 
   // Delay to wait for the .NET backend to be ready
   setTimeout(createWindow, 3000);
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+// Quit when all windows are closed, except on macOS. There, it's common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
